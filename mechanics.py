59a3f1ad397846d6ff281615cf019687b6176119
Aleksander Kot <aleksanderkot@outlook.com>
2026-04-24 23:36:22 +0200

    Rzuty kością

--- mechanics.py
+++ mechanics.py
@@ -1,1 +1,16 @@
-# Testowy commit - [Twoje imię]
+import random
+
+
+def rzut_koscia():
+    return random.randint(1, 20)
+
+
+def sprawdz_prog(wynik, prog=12):
+    if wynik >= prog:
+        return True
+    else:
+        return False
+
+
+moj_rzut = rzut_koscia()
+sukces = sprawdz_prog(moj_rzut)
